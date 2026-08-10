a09c8a2d52648830d6b6f89436de7d98e5ff4d9c
Abhishek Patil <abhishekpatil1064@gmail.com>
2026-08-10 23:26:56 +0530

    Initial commit - Base Spring Boot & Firebase Setup

---
+++ src/main/java/com/hiresphere/hiresphere/HiresphereApplication.java
@@ -0,0 +1,13 @@
+package com.hiresphere.hiresphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiresphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiresphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiresphere/hiresphere/HiresphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiresphere.hiresphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiresphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
